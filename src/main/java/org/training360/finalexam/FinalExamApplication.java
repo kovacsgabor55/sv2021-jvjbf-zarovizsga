304bba6da7ebf879f2ebe071b17b205f01f0d5ef
ShadowDragon <kovacsgabor55@gmail.com>
2021-08-09 17:22:10 +0200

    rename main class

--- src/main/java/org/training360/finalexam/FinalExamApplication.java
+++ src/main/java/org/training360/finalexam/FinalExamApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Sv2021JvjbfZarovizsgaApplication {
+public class FinalExamApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Sv2021JvjbfZarovizsgaApplication.class, args);
+        SpringApplication.run(FinalExamApplication.class, args);
     }
 
     @Bean
